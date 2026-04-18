9ee07e6480ed54a2afef4faa12f0d02896f55256
VionaHumolli <viona.humolli@gmail.com>
2026-04-18 22:44:45 +0200

    updated server

--- server.cpp
+++ server.cpp
@@ -1,84 +1,91 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
-#include <string>
+#include <thread>
+#include <atomic>
 
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
-int main() {
-    WSADATA wsa;
-    SOCKET serverSocket = INVALID_SOCKET;
-    SOCKET clientSocket = INVALID_SOCKET;
-    sockaddr_in serverAddr = {};
-    sockaddr_in clientAddr = {};
-    int clientSize = sizeof(clientAddr);
+const int PORT = 54000;
+const int MAX_CLIENTS = 3;
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        cout << "WSAStartup failed: " << WSAGetLastError() << endl;
-        return 1;
-    }
+atomic<int> activeClients(0);
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == INVALID_SOCKET) {
-        cout << "Socket creation failed: " << WSAGetLastError() << endl;
-        WSACleanup();
-        return 1;
-    }
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(54000);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;   // Listen on all interfaces (better than inet_pton for 0.0.0.0)
-
-    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        cout << "Bind failed: " << WSAGetLastError() << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        cout << "Listen failed: " << WSAGetLastError() << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    cout << "Server running on port 54000...\n";
-
-
-    clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
-    if (clientSocket == INVALID_SOCKET) {
-        cout << "Accept failed: " << WSAGetLastError() << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-  
+void handleClient(SOCKET clientSocket, sockaddr_in clientAddr) {
     char clientIP[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
-    cout << "Client connected from: " << clientIP << endl;
 
- 
-    char buffer[1024] = {};
-    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+    cout << "Client connected: " << clientIP << endl;
 
-    if (bytesReceived > 0) {
+    char buffer[1024];
+
+    while (true) {
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytesReceived <= 0) {
+            cout << "Client disconnected: " << clientIP << endl;
+            break;
+        }
+
         buffer[bytesReceived] = '\0';
-        cout << "Client says: " << buffer << endl;
-
+        cout << "[" << clientIP << "]: " << buffer << endl;
 
         const char* reply = "Hello from server!";
         send(clientSocket, reply, strlen(reply), 0);
     }
 
-
     closesocket(clientSocket);
+    activeClients--;  
+}
+
+int main() {
+    WSADATA wsa;
+    SOCKET serverSocket;
+    sockaddr_in serverAddr{}, clientAddr{};
+    int clientSize = sizeof(clientAddr);
+
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, SOMAXCONN);
+
+    cout << "Server running on port " << PORT << endl;
+
+    while (true) {
+        SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
+
+        if (clientSocket == INVALID_SOCKET) {
+            cout << "Accept failed\n";
+            continue;
+        }
+
+
+        if (activeClients >= MAX_CLIENTS) {
+            cout << "Server full → rejecting client\n";
+
+            const char* msg = "Server is full. Connection rejected.\n";
+            send(clientSocket, msg, strlen(msg), 0);
+
+            closesocket(clientSocket);
+            continue;
+        }
+
+ 
+        activeClients++;
+
+        thread t(handleClient, clientSocket, clientAddr);
+        t.detach();
+    }
+
     closesocket(serverSocket);
     WSACleanup();
-
     return 0;
 }
