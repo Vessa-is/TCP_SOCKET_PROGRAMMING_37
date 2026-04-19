7d77da000a073df62fa5c319ea8044312794b692
VleraKelmeendi <vlera.kelmendi3@student.uni-pr.edu>
2026-04-19 23:31:54 +0200

    including admin logic

--- client3.cpp
+++ client3.cpp
@@ -1,6 +1,9 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <cstddef>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -40,9 +43,71 @@ int main() {
     if (msg == "exit") break;
 
     send(sock, msg.c_str(), msg.size(), 0);
+    if (msg.rfind("/upload ", 0) == 0) {
+
+    string filename = msg.substr(8);
+
+    ifstream file(filename, ios::binary);
+    if (!file) {
+        cout << "File not found locally.\n";
+        continue;
+    }
+
+    file.seekg(0, ios::end);
+    size_t size = file.tellg();
+    file.seekg(0);
+
+    // send size
+    send(sock, (char*)&size, sizeof(size), 0);
+
+    char buffer[1024];
+    while (!file.eof()) {
+        file.read(buffer, sizeof(buffer));
+        send(sock, buffer, file.gcount(), 0);
+    }
+
+    file.close();
+}
 
     char buffer[1024];
     int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (msg.rfind("/download ", 0) == 0) {
+
+    string filename = msg.substr(10);
+
+    size_t fileSize;
+    recv(sock, (char*)&fileSize, sizeof(fileSize), 0);
+
+    ofstream file("downloaded" + filename, ios::binary);
+
+    char buffer[1024];
+    size_t received = 0;
+
+    while (received < fileSize) {
+        int bytes = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) break;
+
+        file.write(buffer, bytes);
+        received += bytes;
+    }
+
+    file.close();
+
+    cout << "File downloaded successfully.\n";
+}
+else {
+    // NORMAL TEXT RESPONSE
+    char buffer[1024];
+    int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+
+    if (bytesReceived <= 0) {
+        cout << "Disconnected from server\n";
+        break;
+    }
+
+    buffer[bytesReceived] = '\0';
+    cout << "Server: " << buffer << endl;
+}
 
     if (bytesReceived <= 0) {
         cout << "Disconnected from server\n";
